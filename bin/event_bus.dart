efebb80a124126bdf2ef53b67c9632604a65d2f0
gaber <moohammed.gaber@gmail.com>
2023-01-15 17:43:30 +0200

    ..

--- bin/event_bus.dart
+++ bin/event_bus.dart
@@ -1,7 +1,10 @@
 import 'package:ddd_event/event/domain_event.dart';
 import 'package:ddd_event/event/domain_event_bus.dart';
 import 'package:ddd_event/event/domain_event_handler.dart';
+import 'package:event_bus_plus/event_bus_plus.dart';
 
 import 'user_logged_event_handler.dart';
 
-class AppEventBus extends DomainAsynchronousEventBus {}
+class AppEventBus extends DomainEventBus {
+  AppEventBus():super(EventBus());
+}
--- lib/event/domain_event_bus.dart
+++ lib/event/domain_event_bus.dart
@@ -1,25 +1,14 @@
 import 'package:ddd_event/event/domain_event.dart';
 import 'package:event_bus_plus/event_bus_plus.dart';
 
-import 'domain_event_handler.dart';
+abstract class DomainEventBus {
+  DomainEventBus(this.eventBus);
 
-abstract class _DomainEventBus {
   final EventBus eventBus;
 
   void fire(DomainEvent event) => eventBus.fire(event);
 
-  void registerHandlers() {
-    eventBus.on<DomainEvent>().listen((event) => event.handler.handle(event));
-  }
-
-
-  _DomainEventBus(this.eventBus);
+  void registerHandlers() =>
+      eventBus.on<DomainEvent>().listen((event) => event.handler.handle(event));
 }
 
-abstract class DomainSynchronousEventBus extends _DomainEventBus {
-  DomainSynchronousEventBus() : super(EventBus());
-}
-
-abstract class DomainAsynchronousEventBus extends _DomainEventBus {
-  DomainAsynchronousEventBus() : super(EventBus());
-}
